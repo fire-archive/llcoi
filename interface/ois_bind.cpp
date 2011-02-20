b2f45dbc80f6ea4ce9a625f66a70497f1b8c2685
jacmoe <jacmoe@mail.dk>
2011-02-20 22:38:53 +0100

    Binding OIS ...

--- interface/ois_bind.cpp
+++ interface/ois_bind.cpp
@@ -54,15 +54,15 @@ void destroy_input_system()
 	OIS::InputManager::destroyInputSystem(input_manager);
 }
 
-MouseInputHandle create_mouse_object()
+MouseInputHandle create_mouse_object(int buffered)
 {
-	OIS::Mouse* mouse = static_cast<OIS::Mouse*>(input_manager->createInputObject( OIS::OISMouse, true ));
+	OIS::Mouse* mouse = static_cast<OIS::Mouse*>(input_manager->createInputObject( OIS::OISMouse, (bool)buffered ));
 	return reinterpret_cast<MouseInputHandle>(mouse);
 }
 
-KeyboardInputHandle create_keyboard_object()
+KeyboardInputHandle create_keyboard_object(int buffered)
 {
-	OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(input_manager->createInputObject( OIS::OISKeyboard, true ));
+    OIS::Keyboard* keyboard = static_cast<OIS::Keyboard*>(input_manager->createInputObject( OIS::OISKeyboard, (bool)buffered ));
 	return reinterpret_cast<KeyboardInputHandle>(keyboard);
 }
 
@@ -76,4 +76,64 @@ void destroy_keyboard_object(KeyboardInputHandle keyboard_handle)
 {
 	OIS::Keyboard* keyboard = reinterpret_cast<OIS::Keyboard*>(keyboard_handle);
 	input_manager->destroyInputObject(keyboard);
+}
+
+int is_key_down(KeyboardInputHandle keyboard_handle, KeyCode key_code)
+{
+    OIS::Keyboard* keyboard = reinterpret_cast<OIS::Keyboard*>(keyboard_handle);
+    if(keyboard->isKeyDown((OIS::KeyCode)key_code))
+        return 1;
+    return 0;
+}
+
+int keyboard_is_modifier_down(KeyboardInputHandle keyboard_handle, const Key_Modifier key_modifier)
+{
+    OIS::Keyboard* keyboard = reinterpret_cast<OIS::Keyboard*>(keyboard_handle);
+    if(keyboard->isModifierDown((OIS::Keyboard::Modifier)key_modifier))
+        return 1;
+    return 0;
+}
+
+const MouseState& mouse_get_state(MouseInputHandle mouse_handle)
+{
+    OIS::Mouse* mouse = reinterpret_cast<OIS::Mouse*>(mouse_handle);
+    OIS::MouseState state = mouse->getMouseState();
+    MouseState out_state;
+    out_state.buttons = state.buttons;
+    out_state.height = state.height;
+    out_state.width = state.width;
+    out_state.X.abs = state.X.abs;
+    out_state.X.rel = state.X.rel;
+    out_state.X.absOnly = state.X.absOnly;
+    out_state.Y.abs = state.Y.abs;
+    out_state.Y.rel = state.Y.rel;
+    out_state.Y.absOnly = state.Y.absOnly;
+    out_state.Z.abs = state.Z.abs;
+    out_state.Z.rel = state.Z.rel;
+    out_state.Z.absOnly = state.Z.absOnly;
+    return out_state;
+}
+
+void mouse_set_buffered(MouseInputHandle mouse_handle, int buffered)
+{
+    OIS::Mouse* mouse = reinterpret_cast<OIS::Mouse*>(mouse_handle);
+    mouse->setBuffered((bool)buffered);
+}
+
+void keyboard_set_buffered(KeyboardInputHandle keyboard_handle, int buffered)
+{
+    OIS::Keyboard* keyboard = reinterpret_cast<OIS::Keyboard*>(keyboard_handle);
+    keyboard->setBuffered((bool)buffered);
+}
+
+void keyboard_capture(KeyboardInputHandle keyboard_handle)
+{
+    OIS::Keyboard* keyboard = reinterpret_cast<OIS::Keyboard*>(keyboard_handle);
+    keyboard->capture();
+}
+
+void mouse_capture(MouseInputHandle mouse_handle)
+{
+    OIS::Mouse* mouse = reinterpret_cast<OIS::Mouse*>(mouse_handle);
+    mouse->capture();
 }
