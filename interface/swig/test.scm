69c52b31fef0b5893cb04748e1d753933ba5e1f0
jacmoe <jacmoe@mail.dk>
2011-03-14 00:11:40 +0100

    Chicken scheme not really happy about windowhandle - causes panic and heap overflows..

--- interface/swig/test.scm
+++ interface/swig/test.scm
@@ -1,4 +1,26 @@
 (load "ogre.so")
 
-(create-root "" "" "")
+(define *windowhandle* 0)
+(define *camerahandle* 0)
+(define *viewporthandle* 0)
+(define *windowhwnd* 0)
+(define *entityhandle* 0)
+(define *nodehandle* 0)
+(define *lighthandle* 0)
+(define *mouse* 0)
+(define *keyboard* 0)
+(define *timer* 0)
+(define *use-mouse* 0)
+
+
+(create-root "plugins.cfg" "ogre.cfg" "ogre.log")
+
+(restore-config)
+
+(setup-resources "resources.cfg")
+
+;;(set! *windowhandle* (root-initialise 1 "therenderwindow"))
+(root-initialise 1 "therenderwindow")
+
 (release-engine)
+
