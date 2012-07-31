61e2c76cd30e896577f059ebe324057575758b47
Jeremy Sandell <jlsandell@gmail.com>
2012-07-31 14:10:22 -0400

    added get/set access to members normal and d

--- interface/plane_bind.cpp
+++ interface/plane_bind.cpp
@@ -57,3 +57,30 @@ void plane_destroy_plane(PlaneHandle handle)
     Ogre::Plane* plane = reinterpret_cast<Ogre::Plane*>(handle);
     delete plane;
 }
+
+void plane_get_normal(PlaneHandle handle, coiVector3* normal)
+{
+    Ogre::Plane* plane = reinterpret_cast<Ogre::Plane*>(handle);
+    Ogre::Vector3 n = plane->normal;
+    normal->x = n.x;
+    normal->y = n.y;
+    normal->z = n.z;
+}
+
+void plane_set_normal(PlaneHandle handle, const coiVector3* normal)
+{
+    Ogre::Plane* plane = reinterpret_cast<Ogre::Plane*>(handle);
+    plane->normal = Ogre::Vector3(normal->x, normal->y, normal->z);
+}
+
+coiReal plane_get_d(PlaneHandle handle)
+{
+    Ogre::Plane* plane = reinterpret_cast<Ogre::Plane*>(handle);
+    return plane->d;
+}
+
+void plane_set_d(PlaneHandle handle, coiReal d)
+{
+    Ogre::Plane* plane = reinterpret_cast<Ogre::Plane*>(handle);
+    plane->d = d;
+}
