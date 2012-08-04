3d4bdfba6f0a73b477da65758f8a457a6f44f58a
Jeremy Sandell <jlsandell@gmail.com>
2012-08-04 18:30:33 -0400

    fix function name

--- interface/ois_interface.h
+++ interface/ois_interface.h
@@ -281,7 +281,7 @@ DLL void mouse_capture(MouseInputHandle mouse_handle);
 
 DLL MouseListenerHandle mouse_set_event_callback(MouseInputHandle handle, MouseMovedEvent moved, MousePressedEvent pressed, MouseReleasedEvent released, void* userdata);
 
-DLL void remove_mouse_listener(MouseInputHandle mouse_handle, MouseListenerHandle handle);
+DLL void mouse_remove_event_callback(MouseInputHandle mouse_handle, MouseListenerHandle handle);
 
 DLL ParamListHandle ois_create_paramlist();
 
