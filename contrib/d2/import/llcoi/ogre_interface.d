0f2183969d65d7ada879461f93fc027398025d3b
Jeremy Sandell <jlsandell@gmail.com>
2012-08-11 19:59:15 -0400

    and exposed them

--- contrib/d2/import/llcoi/ogre_interface.d
+++ contrib/d2/import/llcoi/ogre_interface.d
@@ -731,3 +731,15 @@ AxisAlignedBoxHandle create_axis_aligned_box();
 AxisAlignedBoxHandle create_axis_aligned_box_v3(const ref coiVector3 min, const ref coiVector3 max);
 void destroy_axis_aligned_box(AxisAlignedBoxHandle handle);
 void axisalignedbox_get_size(AxisAlignedBoxHandle handle, ref coiVector3 size);
+void axisalignedbox_get_minimum(AxisAlignedBoxHandle handle, ref coiVector3 minimum);
+void axisalignedbox_get_maximum(AxisAlignedBoxHandle handle, ref coiVector3 maximum);
+void axisalignedbox_set_minimum_x(AxisAlignedBoxHandle handle, coiReal x);
+void axisalignedbox_set_minimum_y(AxisAlignedBoxHandle handle, coiReal y);
+void axisalignedbox_set_minimum_z(AxisAlignedBoxHandle handle, coiReal z);
+void axisalignedbox_set_minimum(AxisAlignedBoxHandle handle, const ref coiVector3 min);
+void axisalignedbox_set_maximum(AxisAlignedBoxHandle handle, const ref coiVector3 max);
+void axisalignedbox_set_maximum_x(AxisAlignedBoxHandle handle, coiReal x);
+void axisalignedbox_set_maximum_y(AxisAlignedBoxHandle handle, coiReal y);
+void axisalignedbox_set_maximum_z(AxisAlignedBoxHandle handle, coiReal z);
+void axisalignedbox_set_extents(AxisAlignedBoxHandle handle, const ref coiVector3 min, const ref coiVector3 max);
+
--- interface/ogre_interface.h
+++ interface/ogre_interface.h
@@ -834,5 +834,16 @@ DLL AxisAlignedBoxHandle create_axis_aligned_box();
 DLL AxisAlignedBoxHandle create_axis_aligned_box_v3(const coiVector3* min, const coiVector3* max);
 DLL void destroy_axis_aligned_box(AxisAlignedBoxHandle handle);
 DLL void axisalignedbox_get_size(AxisAlignedBoxHandle handle, coiVector3* size);
+DLL void axisalignedbox_get_minimum(AxisAlignedBoxHandle handle, coiVector3* minimum);
+DLL void axisalignedbox_get_maximum(AxisAlignedBoxHandle handle, coiVector3* maximum);
+DLL void axisalignedbox_set_minimum_x(AxisAlignedBoxHandle handle, coiReal x);
+DLL void axisalignedbox_set_minimum_y(AxisAlignedBoxHandle handle, coiReal y);
+DLL void axisalignedbox_set_minimum_z(AxisAlignedBoxHandle handle, coiReal z);
+DLL void axisalignedbox_set_minimum(AxisAlignedBoxHandle handle, const coiVector3* min);
+DLL void axisalignedbox_set_maximum(AxisAlignedBoxHandle handle, const coiVector3* max);
+DLL void axisalignedbox_set_maximum_x(AxisAlignedBoxHandle handle, coiReal x);
+DLL void axisalignedbox_set_maximum_y(AxisAlignedBoxHandle handle, coiReal y);
+DLL void axisalignedbox_set_maximum_z(AxisAlignedBoxHandle handle, coiReal z);
+DLL void axisalignedbox_set_extents(AxisAlignedBoxHandle handle, const coiVector3* min, const coiVector3* max);
 
 #endif
