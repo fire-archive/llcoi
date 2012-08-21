7293db1fd497e6d52f40353d06826dc429ed3628
Jeremy Sandell <jlsandell@gmail.com>
2012-08-21 09:55:54 -0400

    added enums orientation_mode, projection_type, frustum_plane from OgreFrustum.h

--- interface/include/ogre_interface.h
+++ interface/include/ogre_interface.h
@@ -281,4 +281,32 @@ typedef enum
 } log_message_level;
 
 
+typedef enum
+{
+    OR_DEGREE_0       = 0,
+    OR_DEGREE_90      = 1,
+    OR_DEGREE_180     = 2,
+    OR_DEGREE_270     = 3,
+
+    OR_PORTRAIT       = OR_DEGREE_0,
+    OR_LANDSCAPERIGHT = OR_DEGREE_90,
+    OR_LANDSCAPELEFT  = OR_DEGREE_270
+} orientation_mode;
+
+typedef enum
+{
+    PT_ORTHOGRAPHIC,
+    PT_PERSPECTIVE
+} projection_type;
+
+typedef enum
+{
+    FRUSTUM_PLANE_NEAR   = 0,
+    FRUSTUM_PLANE_FAR    = 1,
+    FRUSTUM_PLANE_LEFT   = 2,
+    FRUSTUM_PLANE_RIGHT  = 3,
+    FRUSTUM_PLANE_TOP    = 4,
+    FRUSTUM_PLANE_BOTTOM = 5
+} frustum_plane;
+
 #endif
