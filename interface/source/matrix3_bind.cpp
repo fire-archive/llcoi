a056d49be7f6003d9d3dd26106e9f31639f63845
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-09 23:45:21 -0800

    Finish matrix3FromEulerAnglesYXZ().

--- interface/source/matrix3_bind.cpp
+++ interface/source/matrix3_bind.cpp
@@ -13,8 +13,21 @@
 coiMatrix3 matrix_3_from_euler_angles_Y_X_Z( coiRadian y_angle, 
 	coiRadian x_angle, coiRadian z_angle) 
 {
-	Ogre::Matrix3* mat = new Ogre::Matrix3
-	mat->FromEulerAnglesYXZ(y_angle, x_angle, z_angle);
+	Ogre::Matrix3* mat = new Ogre::Matrix3;
+	Ogre::Radian yangle(y_angle), xangle(x_angle), zangle(z_angle);
 	
-	return mat->m;
+	mat->FromEulerAnglesYXZ(yangle, xangle, zangle);
+	coiMatrix3 coimat;
+	coimat.m[0][0] = *mat[0][0]; 
+	coimat.m[0][1] = *mat[0][1];
+	coimat.m[0][2] = *mat[0][2];
+	coimat.m[1][0] = *mat[1][0];   
+	coimat.m[1][1] = *mat[1][1];
+	coimat.m[1][2] = *mat[1][2];
+	coimat.m[2][0] = *mat[2][0];
+	coimat.m[2][1] = *mat[2][1];
+	coimat.m[2][2] = *mat[2][2]; 
+	delete mat;     
+	 
+	return coimat;
 }
