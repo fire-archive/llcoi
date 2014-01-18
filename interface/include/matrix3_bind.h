404508fdde1baaff8ed0e1db7ff73ca109f37fe8
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-18 13:08:13 -0800

    Wrong function signature.

--- interface/include/matrix3_bind.h
+++ interface/include/matrix3_bind.h
@@ -13,6 +13,6 @@
 
 DLL coiMatrix3 matrix_3_from_euler_angles_y_x_z( coiRadian y_angle, 
 	coiRadian x_angle, coiRadian z_angle);
-DLL void matrix_3_to_euler_angles_y_x_z(coiMatrix3 mat, coiRadian y_angle, 
-	coiRadian x_angle, coiRadian z_angle);
+DLL void matrix_3_to_euler_angles_y_x_z(coiMatrix3 mat, coiRadian *y_angle, 
+	coiRadian *x_angle, coiRadian *z_angle);
 	
