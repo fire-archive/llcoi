cc5c6808afe1c649b7e962fdbd1cd41507af4fc7
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-23 17:36:51 -0800

    Add vector2 includes.

---
+++ interface/source/vector2_bind.cpp
@@ -0,0 +1,11 @@
+/*     __ __              _ 
+ *    / // /_____ ____   (_)
+ *   / // // ___// __ \ / / 
+ *  / // // /__ / /_/ // /  
+ * /_//_/ \___/ \____//_/   
+ * https://bitbucket.org/galaktor/llcoi 
+ * copyright (c) 2014, llcoi Team
+ * MIT license applies - see file "LICENSE" for details.
+*/
+#include "ogre_interface.h"
+#include <OgreVector2.h>
