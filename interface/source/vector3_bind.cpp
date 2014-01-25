b24170d8bdc8270954434fa017c60bd1cd0b7d69
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-24 20:07:54 -0800

    Wrapped more vector3.

--- interface/source/vector3_bind.cpp
+++ interface/source/vector3_bind.cpp
@@ -10,61 +10,96 @@
 #include "vector3_bind.h"
 #include <OgreVector3.h>
 
+//Vector3::Vector3()
+
+Vector3Handle vector3_create() {
+    Ogre::Vector3 *vector3 = new Ogre::Vector3();
+    return reinterpret_cast<Vector3Handle>(vector3);
+}
+
+coiReal vector3_x(Vector3Handle handle) {
+    Ogre::Vector3 *vector3 = reinterpret_cast<Ogre::Vector3*>(handle);
+    return coiReal(vector3->x);
+}
+
+coiReal vector3_y(Vector3Handle handle) {
+    Ogre::Vector3 *vector3 = reinterpret_cast<Ogre::Vector3*>(handle);
+    return coiReal(vector3->y);
+}
+
+coiReal vector3_z(Vector3Handle handle) {
+    Ogre::Vector3 *vector3 = reinterpret_cast<Ogre::Vector3*>(handle);
+    return coiReal(vector3->z);
+}
+
 //Vector3::operator !=
-int vector3_notequals_vector3(coiVector3& lhs, coiVector3& rhs)
+int vector3_notequals_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
-    return  ( lhs.x != rhs.x || lhs.y != rhs.y || lhs.z != rhs.z );
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs);
+    return lhs_bind->operator!=(*rhs_bind);
 }
 
 //Vector3::operator ==
-int vector3_equals_vector3(coiVector3& lhs, coiVector3& rhs)
+int vector3_equals_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
-    return  ( lhs.x == rhs.x || lhs.y == rhs.y || lhs.z == rhs.z );
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs);  
+    return lhs_bind->operator==(*rhs_bind);
 }
 
 //Vector3::operator +
-coiVector3 vector3_add_vector3(coiVector3& lhs, coiVector3& rhs)
+coiVector3 vecotr3_add_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs);  
+    Ogre::Vector3 temp = lhs_bind->operator+(*rhs_bind);
     coiVector3 result;
-    result.x = lhs.x + rhs.x;
-    result.y = lhs.y + rhs.y;
-    result.z = lhs.z + rhs.z;
+    result.x = temp.x;
+    result.y = temp.y;
+    result.z = temp.z;
     return result;
 }
 
 //Vector3::operator +=
-void vector3_update_add_vector3(coiVector3& lhs, coiVector3& rhs)
+Vector3Handle vector3_update_add_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
-    lhs.x += rhs.x;
-    lhs.y += rhs.y;
-    lhs.z += rhs.z;
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs); 
+    lhs_bind->operator+=(*rhs_bind); 
+    return reinterpret_cast<Vector3Handle>(lhs_bind);
 }
 
 //Vector3::operator -
-coiVector3 vector3_subtract_vector3(coiVector3& lhs, coiVector3& rhs)
+coiVector3 vector3_subtract_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs);  
+    Ogre::Vector3 temp = lhs_bind->operator-(*rhs_bind);
     coiVector3 result;
-    result.x = lhs.x - rhs.x;
-    result.y = lhs.y - rhs.y;
-    result.z = lhs.z - rhs.z;
+    result.x = temp.x;
+    result.y = temp.y;
+    result.z = temp.z;
     return result;
 }
 
 //Vector3::operator -=
-void vector3_update_subtract_vector3(coiVector3& lhs, coiVector3& rhs)
+void vector3_update_subtract_vector3(Vector3Handle lhs, Vector3Handle rhs)
 {
-    lhs.x -= rhs.x;
-    lhs.y -= rhs.y;
-    lhs.z -= rhs.z;
+    Ogre::Vector3 *lhs_bind = reinterpret_cast<Ogre::Vector3*>(lhs);
+    Ogre::Vector3 *rhs_bind = reinterpret_cast<Ogre::Vector3*>(rhs);  
+    lhs_bind->operator-=(*rhs_bind);
 }
 
 //Vector3::operator - 
-coiVector3 vector3_negate(coiVector3& v3)
+coiVector3  vector3_negate(Vector3Handle handle)
 {
+    Ogre::Vector3 *vec = reinterpret_cast<Ogre::Vector3*>(handle);
+    vec->operator-();
     coiVector3 result;
-    result.x = -v3.x;
-    result.y = -v3.y;
-    result.z = -v3.z;
+    result.x = vec->x;
+    result.y = vec->y;
+    result.z = vec->z;
     return result;
 }
 
