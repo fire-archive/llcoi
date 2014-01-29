fe1f93f0d722eeb98455209e93eaf9ccd8db5dad
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-01-29 01:30:19 -0800

    Forgot to add to .h.

--- interface/include/quaternion_bind.h
+++ interface/include/quaternion_bind.h
@@ -25,3 +25,4 @@ DLL QuaternionHandle quaternion_subtract_quaternion(QuaternionHandle lhs, Quater
 DLL QuaternionHandle quaternion_unit_inverse(QuaternionHandle q);
 DLL QuaternionHandle quaternion_multiply_scalar(QuaternionHandle lhs, coiReal scalar);
 DLL coiReal quaternion_normalise(QuaternionHandle q);
+DLL void quaternion_to_angle_axis_degree(QuaternionHandle q, coiDegree dAngle, Vector3Handle Axis);
